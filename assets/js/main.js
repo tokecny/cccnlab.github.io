40e414cae6f1c3f4e363e67a36c676dba457edcd
Thiti Chainiyom <97273062+tokecny@users.noreply.github.com>
2022-12-27 17:14:13 +0700

    stop auto slide in people & alumni

--- assets/js/main.js
+++ assets/js/main.js
@@ -163,7 +163,7 @@
 
   // Testimonials carousel (uses the Owl Carousel library)
   $(".testimonials-carousel").owlCarousel({
-    autoplay: true,
+    autoplay: false,
     dots: true,
     loop: true,
     responsive: {
